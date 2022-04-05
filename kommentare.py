7057fda1aaad1bf39dc8c40f1ae804818013e34a
Emre Sarak <emre.sarak@outlook.de>
2022-04-05 20:12:38 +0200

    Programm zu Kommentaren in Python

--- kommentare.py
+++ kommentare.py
@@ -1,13 +1,12 @@
 #Kommentare können auf zwei Wegen erfolgen. 
-#1. mit einem ' # '. Die Zeichen, die nach dem # folgen dienen als Kommentar. Auf diese Art und Weise kann man ein Kommentar verfassen. 
-#2. es werden Strings genutzt die nicht initialisiert werden. Diese können vom Nutzer gelesen werden, haben jedoch keinen Einfluss auf die Funktion.
+#1. mit einem ' # '. Die Zeichen, die nach dem # folgen dienen als Kommentar. Auf diese Art und Weise kann man ein Kommentar verfassen,welcher sich über eine Zeile erstreckt. 
+#2. es werden Strings genutzt die nicht initialisiert werden. Diese können vom Nutzer gelesen werden, haben jedoch keinen Einfluss auf die Funktion, da sie nicht einer Variablen zugewiesen werden.
 
 
 #Bsp. zu 1:
 
-#Ausgabe von "Hello World"
-print("Hello World")
-print("Alles was nach dem der print-Anweisung folgt ist ein Kommentar") #Kommentar geht hier weiter
+#Kommentar_1
+print("Alles was nach dem der print-Anweisung folgt ist ein Kommentar") #Kommentar_1 geht nach der print-Anweisung hier weiter
 
 
 #Bsp. zu 2:
