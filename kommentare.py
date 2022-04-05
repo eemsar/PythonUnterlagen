a3facdf297c91ab1d4c96762c179ca63facb6f94
Emre Sarak <emre.sarak@outlook.de>
2022-04-05 19:43:07 +0200

    Beispiele für Kommentare in Python

---
+++ kommentare.py
@@ -0,0 +1,30 @@
+#Kommentare können auf zwei Wegen erfolgen. 
+#1. mit einem ' # '. Die Zeichen, die nach dem # folgen dienen als Kommentar. Auf diese Art und Weise kann man ein Kommentar verfassen. 
+#2. es werden Strings genutzt die nicht initialisiert werden. Diese können vom Nutzer gelesen werden, haben jedoch keinen Einfluss auf die Funktion.
+
+
+#Bsp. zu 1:
+
+#Ausgabe von "Hello World"
+print("Hello World")
+print("Alles was nach dem der print-Anweisung folgt ist ein Kommentar") #Kommentar geht hier weiter
+
+
+#Bsp. zu 2:
+
+'Das ist ein Kommentar'
+
+"Das ist ebenfalls ein Kommentar"
+
+"""Das 
+    ist 
+    ein 
+    Mehrzeilenkommentar
+"""
+    
+'''Das 
+    ist 
+    ebenfalls
+    ein 
+    Mehrzeilenkommentar
+'''
