8492207c4e5ab99b7e59de8742770493655b0817
Emre Sarak <emre.sarak@outlook.de>
2022-04-06 14:03:16 +0200

    Basics zu Variablen

---
+++ variablen.py
@@ -0,0 +1,20 @@
+#In Python müssen allen Variablen bei der Erstellung eine Wertzuweisung erfolgen, ansonsten wird die Variable verworfen.
+#Datentypen müssen bei der Initialisierung nicht angegeben werden.
+#Variablennamen die aus mehreren Namen bestehen nutzen die Camelcase (nameDerVariable), Pascalcase (NameDerVariable) oder/und die Snakecase (name_der_variable)
+
+
+#Zahlen
+intzahl = 20
+gleitkommazahl = 2.22
+print(str(intzahl) + " & " + str(gleitkommazahl))
+
+ 
+#Boolean 
+boolTrue = True
+boolFalse = False
+print('\n' + str(boolTrue) + " & " + str(boolFalse))
+
+
+#Strings
+stringValue = "\nHello World"
+print(stringValue)
